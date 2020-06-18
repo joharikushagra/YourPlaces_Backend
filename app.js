ab5a334e1484645480f8b49b9fc791673c238033
Kushagra Johari <skjkushagra1309@gmail.com>
2020-06-18 17:56:55 +0530

    last changes made

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "an unknown error occured!" });
 });
 
-mongoose.connect('mongodb+srv://kushagra_13:RgI3zlVMR8sAQEI1@cluster0-w2lg8.mongodb.net/mern?retryWrites=true&w=majority').then(()=>{
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-w2lg8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`).then(()=>{
     app.listen(5000, () => {
         console.log("server started");
       });
